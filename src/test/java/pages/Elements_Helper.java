fe59c8937c8c75377cda15bbfb432b084aaf5cc1
guru <gurunath.hd@gmail.com>
2018-02-02 13:29:49 +1100

    captured elements of the page using selenium and native javascript

---
+++ src/test/java/pages/Elements_Helper.java
@@ -0,0 +1,7 @@
+package pages;
+
+/**
+ * Created by guru on 2/02/18.
+ */
+public class Elements_Helper {
+}
--- src/test/java/steps/HorseRace.java
+++ src/test/java/steps/HorseRace.java
@@ -3,7 +3,9 @@ package steps;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import org.junit.Before;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
@@ -19,12 +21,13 @@ public class HorseRace {
 
     WebDriver driver;
     WilliamHillHomePage objHomePage;
+    String baseUrl = "https://www.williamhill.com.au/";
 
     @cucumber.api.java.Before
     public void setup() {
         driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://www.williamhill.com.au/");
+        driver.get(baseUrl);
     }
 
     @Given("^I am on William Hill main page$")
@@ -32,8 +35,93 @@ public class HorseRace {
         objHomePage = new WilliamHillHomePage(driver);
         objHomePage.click("asdf");
 
+//driver.findElement(By.cssSelector("div[class^=Header_header] h1")).getText()
+//"Sports Betting Online & Horse Racing in Australia - William Hill"
+        //"Horse Racing Odds & Online Betting at William Hill"
+
         //driver.findElement(By.linkText("RACING")).click();
         //driver.findElements(By.cssSelector("div[class^=EventTypeHeader]")).get(0).findElement(By.tagName("span")).getText()
+        //driver.findElements(By.cssSelector(".Grid_table_1dV")).get(1).findElements(By.tagName("a")).get(0).click();
+        //
+        //driver.findElements(By.cssSelector("#mystery-bet svg")).get(1).click();
+        //driver.findElement(By.cssSelector(".Portal_portal_11Y")).findElements(By.tagName("span")).get(4).click();
+//        driver.findElement(By.cssSelector("#mystery-bet")).findElement(By.tagName("input")).clear();
+//        driver.findElement(By.cssSelector("#mystery-bet")).findElement(By.tagName("input")).sendKeys("9");
+
+
+/*
+    - set text 10.5
+
+    JavascriptExecutor js = (JavascriptExecutor) driver;
+WebElement e = (WebElement)((JavascriptExecutor)driver).executeScript("" +
+        "return document.querySelector('#mystery-bet input')"
+);
+js.executeScript("arguments[0].value = 10.5", e);
+ */
+
+        /*
+        WebElement e = (WebElement)((JavascriptExecutor)driver).executeScript(
+                "{var aTags = document.querySelectorAll('#mystery-bet button');\n" +
+                        "    var searchText = \"Add to bet slip\";\n" +
+                        "    var found;\n" +
+                        "\n" +
+                        "    for (var i = 0; i < aTags.length; i++) {\n" +
+                        "      if (aTags[i].textContent == searchText) {\n" +
+                        "        found = aTags[i];\n" +
+                        "        return found;\n" +
+                        "        break;\n" +
+                        "      }\n" +
+                        "    }}"
+        );
+        e.click();
+        */
+
+
+        //driver.findElement(By.cssSelector("span[class^=SideBar_badge]")).getText()
+
+
+        /*
+        WebElement e = (WebElement)((JavascriptExecutor)driver).executeScript(
+                "{var lst = document.querySelectorAll('[class^=SideBar_header] span');\n" +
+                        "    var searchText = \"Bet slip\";\n" +
+                        "    var found;\n" +
+                        "\n" +
+                        "    for (var i = 0; i < lst.length; i++) {\n" +
+                        "      if (lst[i].textContent == searchText) {\n" +
+                        "        found = lst[i];\n" +
+                        "        return found;\n" +
+                        "        break;\n" +
+                        "      }\n" +
+                        "    }}"
+        );
+        e.click();
+         */
+
+        //driver.findElement(By.cssSelector("div[class^=ExoticHeader_title]")).getText()
+
+        /*
+        WebElement e = (WebElement)((JavascriptExecutor)driver).executeScript(
+                "{var lst = document.querySelectorAll('[class^=Betslip_field]');\n" +
+                        "    var searchText = 'Total Cost';\n" +
+                        "    var found;\n" +
+                        "\n" +
+                        "    for (var i = 0; i < lst.length; i++) {\n" +
+                        "      if (lst[i].textContent == searchText) {\n" +
+                        "        found = lst[i].nextElementSibling;\n" +
+                        "        return found;\n" +
+                        "        break;\n" +
+                        "      }\n" +
+                        "    }}"
+        );
+        e.getText();
+         */
+
+        /*
+        //  This statement returns the first button-element with in the summary-row-class-area-elements.
+    driver.findElement(By.cssSelector("[class^=Betslip_summaryRow] button")).click();
+         */
+
+
 
     }
 
